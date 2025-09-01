4ab7c41d758542baa1150e50a35b7a487949ce78
Orkun Dayı <orkundayi@flutech.org>
2025-09-01 15:04:11 +0300

    main
    main

--- CvMaker/flutter_bootstrap.js
+++ CvMaker/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3844499349"
+    serviceWorkerVersion: "1272567408"
   }
 });
